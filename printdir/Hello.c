37d31e0fd34bdf489fed672442162ce14fd0e5f1
justinjhjung <snujustin12@gmail.com>
2019-02-04 23:49:04 +0900

    basic operations visited

---
+++ printdir/Hello.c
@@ -0,0 +1,15 @@
+/*
+제목 : 내 이름 출력하기
+기능 : 문자열 출력
+파일 이름 : Helloworld.c
+수정 날짜 : 2019.02.04
+작성자 : 정재훈
+*/
+
+#include <stdio.h> //헤더파일 선언
+
+int main(void) // main 함수 시작
+{
+	printf("정재훈 \n 정 재훈\n 정 재 훈");
+	return 0;
+}  // main 함수의 끝 
---
+++ printdir/Printftwo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("제 이름은 정재훈입니다.\n제 나이는 %d 살이고요.\n제가 사는 곳의 번지수는 %d-%d입니다.\n", 28, 123, 456);
+	printf("%dX%d=%d\n",4,5,20);
+       	printf("%dX%d=%d\n",7,9,63);
+	return 0;
+}	
